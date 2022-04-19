606798f5f44be841d788272c9daba30e2f55c0d6
Moises Santillan <moises.santillan.zeron@gmail.com>
2022-04-19 09:06:33 -0500

    $

--- Code/covid19.py
+++ Code/covid19.py
@@ -1,10 +1,10 @@
 import pandas as pd
-import os
+import numpy as np
 import matplotlib.pyplot as plt
 import plotly.express as px
 from plotly.subplots import make_subplots
-from statsmodels.tsa.ar_model import AutoReg
-
+from scipy.signal import savgol_filter
+from scipy.stats import norm
 
 
 def count_incidence(df, date, entity):
@@ -19,14 +19,14 @@ def count_incidence(df, date, entity):
 os.system("wget https://datosabiertos.salud.gob.mx/gobmx/salud/datos_abiertos/datos_abiertos_covid19.zip")
 os.system("unzip datos_abiertos_covid19.zip")
 os.system("rm datos_abiertos_covid19.zip")
-
 file = (pd.to_datetime('today') - pd.Timedelta('1 days')).strftime('%y%m%d')+"COVID19MEXICO.csv" #Database file
-Lag = 365#1 year into the past
-Date_Range = pd.date_range(end=pd.to_datetime('today').date(), periods=Lag)
-
 df = pd.read_csv(file, engine="python")
 os.system("rm "+file)
 
+
+
+Lag = 90
+Date_Range = pd.date_range(end=pd.to_datetime('today').date(), periods=Lag)
 columns = list(df.columns)
 columns.remove('ENTIDAD_RES')
 columns.remove('FECHA_SINTOMAS')
@@ -48,34 +48,27 @@ for entity in range(33):
 df_Incidence.to_csv("../Data/Incidence.csv", index=False)
 
 
-
-loc = (pd.to_datetime(df['FECHA_INGRESO'])-pd.to_datetime(df['FECHA_SINTOMAS'])).mean().total_seconds()/3600/24 + 3
-scale = (pd.to_datetime(df['FECHA_INGRESO'])-pd.to_datetime(df['FECHA_SINTOMAS'])).std().total_seconds()/3600/24
 Entities = list(df_Entities.Entidad)
-trst = 15
 df_Nowcasting = pd.DataFrame()
 df_Nowcasting["Date"] = df_Incidence["Date"]
+trst = 12
+loc = (pd.to_datetime(df['FECHA_INGRESO'])-pd.to_datetime(df['FECHA_SINTOMAS'])).mean().total_seconds()/3600/24 + 3
+scale = (pd.to_datetime(df['FECHA_INGRESO'])-pd.to_datetime(df['FECHA_SINTOMAS'])).std().total_seconds()/3600/24
 for entity in Entities:
     aux = df_Incidence[entity].rolling(5, center=True).mean()
     aux[[0, 1, Lag-2, Lag-1]] = df_Incidence[entity][[0, 1, Lag-2, Lag-1]]
-    model_fit = AutoReg(aux[:-trst], lags=10, trend='t', seasonal=True, period= 7).fit()
+    aux /= norm.cdf(range(Lag-1, -1, -1), loc=loc, scale=scale)
     df_Nowcasting[entity] = aux
-    prediction = model_fit.predict(start=Lag-trst, end=Lag-1)
-    for i in range(Lag-trst, Lag):
-        df_Nowcasting.at[i, entity] = max(prediction.at[i], df_Nowcasting.at[i, entity])
-df_Nowcasting.to_csv("../Data/Nowcasting.csv", index=False)
-
 
 
 Incidences = []
 Rates = []
 for entity in Entities:
-    Incidences.append(df_Nowcasting[entity][Lag-7:].sum())
-    Rates.append(df_Nowcasting[entity][Lag-7:].sum() / df_Nowcasting[entity][Lag-14:Lag-7].sum() - 1)
+    Incidences.append(df_Nowcasting[entity][Lag-10:-3].sum())
+    Rates.append(df_Nowcasting[entity][Lag-10:-3].sum() / df_Nowcasting[entity][Lag-17:Lag-10].sum() - 1)
 df_Entities["Incidencia Semanal"] = Incidences
 df_Entities['Tasa de Cambio'] = Rates
 df_Entities["Incidencia Semanal Normalizada"] = df_Entities["Incidencia Semanal"] / df_Entities["Población"] * 1e5
-
 
 
 fig, axs = plt.subplots(nrows=2, ncols=1, figsize=(12, 8))
@@ -98,7 +91,6 @@ fig.savefig('../docs/Fig01.png')
 
 fig1 = px.bar(df_Entities, x='Entidad', y='Incidencia Semanal Normalizada')
 fig2 = px.bar(df_Entities, x='Entidad', y='Tasa de Cambio')
-
 fig = make_subplots(rows=2, cols=1)
 fig.add_trace(fig1['data'][0], row=1, col=1)
 fig.add_trace(fig2['data'][0], row=2, col=1)
@@ -108,8 +100,4 @@ fig.update_layout(title_text="Útlima actialización: "+pd.to_datetime('today') 
 fig.update_xaxes(showticklabels=False, title=None, row=1, col=1)
 fig.update_yaxes(title="Incidencia Semanal Normalizada", row=1, col=1)
 fig.update_yaxes(title="Tasa de Cambio", row=2, col=1)
-
-#fig.show()
 fig.write_html("../docs/Fig01.html")
-
-
