4aafe18813a2c935db990d55fc485bff1c752412
Moises Santillan <moises.santillan.zeron@gmail.com>
2022-04-22 02:58:06 -0500

    $

--- Code/covid19.py
+++ Code/covid19.py
@@ -39,14 +39,14 @@ df = df.drop(df[df.CLASIFICACION_FINAL > 3].index)
 df = df.drop(columns=['CLASIFICACION_FINAL'])
 
 
-df_Entities = pd.read_csv("../Data/Entidades.csv")
+df_Entities = pd.read_csv("Data/Entidades.csv")
 df_Incidence = pd.DataFrame({'Date':Date_Range})
 for entity in range(33):
     Incidence = []
     for date in range(Lag):
         Incidence.append(count_incidence(df, str(Date_Range[date].date()), entity) )
     df_Incidence[df_Entities.iloc[entity, 0]] = Incidence
-df_Incidence.to_csv("../Data/Incidence.csv", index=False)
+df_Incidence.to_csv("Data/Incidence.csv", index=False)
 
 
 Entities = list(df_Entities.Entidad)
@@ -60,13 +60,14 @@ for entity in Entities:
     aux[[0, Lag-1]] = df_Incidence[entity][[0, Lag-1]]
     aux /= norm.cdf(range(Lag-1, -1, -1), loc=loc, scale=scale)
     df_Nowcasting[entity] = aux
+df_Nowcasting.to_csv("Data/Nowcasting.csv", index=False)
 
 
 Incidences = []
 Rates = []
 for entity in Entities:
-    Incidences.append(df_Nowcasting[entity][Lag-11:-4].sum())
-    Rates.append(df_Nowcasting[entity][Lag-11:-4].sum() / df_Nowcasting[entity][Lag-18:Lag-11].sum() - 1)
+    Incidences.append(df_Nowcasting[entity][Lag-18:-4].sum())
+    Rates.append(df_Nowcasting[entity][Lag-18:-4].sum() / df_Nowcasting[entity][Lag-32:Lag-18].sum() - 1)
 df_Entities["Incidencia Semanal"] = Incidences
 df_Entities['Tasa de Cambio'] = Rates
 df_Entities["Incidencia Semanal Normalizada"] = df_Entities["Incidencia Semanal"] / df_Entities["Población"] * 1e5
@@ -86,7 +87,7 @@ axs[1].xaxis.set_tick_params(labelsize=13)
 axs[1].yaxis.set_tick_params(labelsize=13)
 axs[1].legend(fontsize=13)
 fig.tight_layout()
-fig.savefig('../docs/Fig01.png')
+fig.savefig('docs/Fig01.png')
 
 
 
@@ -101,4 +102,6 @@ fig.update_layout(title_text="Útlima actialización: "+pd.to_datetime('today') 
 fig.update_xaxes(showticklabels=False, title=None, row=1, col=1)
 fig.update_yaxes(title="Incidencia Semanal Normalizada", row=1, col=1)
 fig.update_yaxes(title="Tasa de Cambio", row=2, col=1)
-fig.write_html("../docs/Fig01.html")
+fig.write_html("docs/Fig01.html")
+
+os.system('Git "update"')
