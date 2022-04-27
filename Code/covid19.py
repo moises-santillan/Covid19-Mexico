5bc19578ac0e0a44853b540588a2ceebb67a38d3
Moises Santillan <moises.santillan.zeron@gmail.com>
2022-04-27 09:19:42 -0500

    $

--- Code/covid19.py
+++ Code/covid19.py
@@ -17,10 +17,10 @@ def count_incidence(df, date, entity):
 
 
 
-os.system("wget https://datosabiertos.salud.gob.mx/gobmx/salud/datos_abiertos/datos_abiertos_covid19.zip")
+os.system("wget -O datos_abiertos_covid19.zip -i https://datosabiertos.salud.gob.mx/gobmx/salud/datos_abiertos/historicos/2022/04/datos_abiertos_covid19_26.04.2022.zip")
 os.system("unzip datos_abiertos_covid19.zip")
 os.system("rm datos_abiertos_covid19.zip")
-file = (pd.to_datetime('today') - pd.Timedelta('2 days')).strftime('%y%m%d')+"COVID19MEXICO.csv" #Database file
+file = (pd.to_datetime('today') - pd.Timedelta('1 days')).strftime('%y%m%d')+"COVID19MEXICO.csv" #Database file
 df = pd.read_csv(file, engine="python")
 os.system("rm "+file)
 
